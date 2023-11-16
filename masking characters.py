9a31fd72f63cf2bfec2d989ca0a830bb206daa12
DIPADYUTI SAHA <129580302+Dipadyuti7@users.noreply.github.com>
2023-11-16 14:29:21 +0530

    Add files via upload
    Masking of last 6 characters of a password.

---
+++ masking characters.py
@@ -0,0 +1,19 @@
+'''p=input("Enter the char to be masked ")#789561238
+v=""
+n=""
+i=len(p)#9
+if i<3:
+    print("masking is not possible")
+else:
+    for j in range(i-6,i):
+        v="*"
+        n=n+v
+print(p[0:i-6]+n)'''
+var = "James" * 2  **3
+
+
+
+
+
+
+
